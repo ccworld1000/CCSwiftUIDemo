12964c692858eec35f7e1bb6f068cff9c134091d
CC (deng you hua) <ccworld1000@gmail.com>
2020-11-01 20:33:24 +0800

    CCAutoTag push : Drawing

---
+++ 4 Drawing Paths and Shapes/Drawing/Drawing/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  Drawing
+//
+//  Created by CC on 2020/11/1.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
